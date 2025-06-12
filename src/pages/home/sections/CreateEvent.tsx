3a4fefa4505dd28d39796493f685583a1450db38
navneetco0 <navneet.dev3@gmail.com>
2025-06-12 14:54:12 +0530

    [Fix]: create event app changed to company name

--- src/pages/home/sections/CreateEvent.tsx
+++ src/pages/home/sections/CreateEvent.tsx
@@ -3,7 +3,7 @@ const CreateEvent = () => {
     <section className="mb-16">
       <div className="bg-gray-800 rounded-lg p-8 flex items-center justify-between">
         <div>
-          <h3 className="text-xl font-bold mb-2">Create an event with App</h3>
+          <h3 className="text-xl font-bold mb-2">Create an event with Cheers be a Slayer</h3>
           <p className="text-gray-300">
             Got a show, event, activity or a great experience? Partner with us &
             get listed on App
